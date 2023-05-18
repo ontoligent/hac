57c20da9eaefd29034b2b5a2137afa0a50aa3c5e
Rafael Alvarado <ontoligent@gmail.com>
2023-05-18 09:24:29 -0400

    Version on copied over and packaged.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HAC",
+    version="1.0",
+    description="A class to generate dendograms from dataframes.", 
+    author="Rafael C. Alvarado",
+    author_email="ontoligent@gmail.com",
+    packages=find_packages()
+)
